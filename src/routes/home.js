16093970470270822439b52cfa4dadebcf277cfd
Ahmed Abdelwadod <me@ahmedwadod.com>
2022-01-21 15:42:54 +0200

    Home router

---
+++ src/routes/home.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+// Create the router
+const router = express.Router()
+
+// Configure the endpoint for the router
+router
+    .get('/', (req, res) => res.send('Hello from Docker!'))
+    .get('/greetings/:name',
+            (req, res) => res.send(`Hello, ${req.params.name}`))
+
+// Export the router
+module.exports = router
